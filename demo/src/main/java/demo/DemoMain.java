e46662fb38e37e4da0ffdbbac3779d4280da3168
이승재 <sjlee@digitalship.co.kr>
2018-12-20 17:40:39 +0900

    초기설정 셋팅

--- demo/src/main/java/demo/DemoMain.java
+++ demo/src/main/java/demo/DemoMain.java
@@ -1,14 +1,10 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootConfiguration
 @EnableAutoConfiguration
-@ComponentScan
 public class DemoMain {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMain.class, args);
